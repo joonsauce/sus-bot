986f715057af129c7a2042b3418583ad1dacb7b8
joonsauce <77248354+joonsauce@users.noreply.github.com>
2021-04-15 17:21:20 -0400

    Moved one-time executes from main file

--- setting.py
+++ setting.py
@@ -1,2 +1,21 @@
+import discord
+import logging
+from discord.ext import commands
+
 prefix = "s!"
-description = "Sus bot, made by joonsauce"
+description = "When the bot is sus!"
+
+# logs bot events
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+# bot setup
+bot = commands.Bot(command_prefix=prefix)
+
+# changes discord status
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game("yo mama"))
