c08de0016e9bf5c7bfdcbe3f1645b9b95879f195
joonsauce <77248354+joonsauce@users.noreply.github.com>
2021-05-25 13:02:22 -0400

    Moved all package imports to setting.py

--- setting.py
+++ setting.py
@@ -1,21 +1,37 @@
+import asyncio
 import discord
 import logging
+import os
+import random
+import requests
+import youtube_dl
 from discord.ext import commands
+from discord.voice_client import VoiceClient
+from requests import get
 
+# sets the prefix to use the bot; use whichever, s! is included as it is what I used
 prefix = "s!"
-description = "When the bot is sus!"
+# sets description of the bot
+description = "Your description here"
 
-# logs bot events
+# bot logging events
+
+# gets discord logger
 logger = logging.getLogger('discord')
+# sets discord logging level as debug
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+# sets the encoding type & specifies file location
+handler = logging.FileHandler(filename='../discord.log', encoding='utf-8', mode='w')
+# formats how the debug file will look like
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+# adds handler
 logger.addHandler(handler)
 
-# bot setup
+# properly sets prefix of bot
 bot = commands.Bot(command_prefix=prefix)
 
-# changes discord status
+# sets discord activity status
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game("yo mama"))
+    # again, add whatever presence you want, I just have set a default one
+    await bot.change_presence(activity=discord.Game("Not venting"))
