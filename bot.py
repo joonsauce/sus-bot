b20276beb5d54eb245dceb8ff2760a6cd4b51484
joonsauce <77248354+joonsauce@users.noreply.github.com>
2021-05-25 13:07:26 -0400

    Added more explanations, fixed p command & added q command

--- bot.py
+++ bot.py
@@ -1,186 +1,97 @@
-import discord
-import random
+# various pacakges to be imported
 from setting import *
-from discord.ext import commands
-from songs import *
-from discord.voice_client import VoiceClient
+from sus import *
+from music import *
 
-# different sus commands are listed below
+# defines next song so songs in queue can be played
+def next_song(ctx):
+    music = discord.utils.get(bot.voice_clients)
+    if len(queue) > 0:
+        # checks if there is a song @ song.mp3 and see if removable
+        song_there = os.path.isfile("song.mp3")
+        if song_there:
+            os.remove("song.mp3")
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            # song gets downloaded then replaced by song.mp3 so bot can play in vc
+            dl = str(links[0])
+            ydl.download([dl])
+            for file in os.listdir("./"):
+                if file.endswith(".mp3"):
+                    os.rename(file, "song.mp3")
+        music.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda x: next_song(ctx))
+        queue.pop(0)
+        links.pop(0)
 
-# susout command; a user accuses another user of something suspicious
-@bot.command()
-async def susout(ctx, arg1, *, arg2):
-    await ctx.send('{0.author.mention} sussed {1} of *{2}*'.format(ctx, arg1, arg2))
-
-# susrate command; rates how sus a person is; is a static value
-@bot.command()
-async def susrate(ctx, *, message):
-    if message == "<@!unique-tag":
-        await ctx.send(message + "'s susrate is 20%")
-
-# different among us game feature commands below
-
-# medbay command; medbay scans user
-@bot.command()
-async def scan(ctx):
-    if ctx.author.mention == "<@!unique-tag>":
-        id = "GREP1"
-        ht = "1.80m"
-        wt = "67kg"
-        c = "Green"
-        bt = "O+"
-        await ctx.send("ID: " + id + " HT: " + ht + " WT: " + wt + " C: " + c + " BT: " + bt)
-
-# different music bot commands below
-
-# ari command; plays random song from playlist; incomplete
-@bot.command()
-async def randomsong(ctx, *, message):
-    if message == "random":
-        song = random.choice(song_list)
-    else:
-        number = int(message)
-        song = song_list[number]
-    try:
-        channel = ctx.author.voice.channel
-        await channel.connect()
-    except discord.errors.ClientException:
-        await ctx.send("now playing: {0}".format(song))
-    except AttributeError:
-        await ctx.send("You're not connected to a voice channel.")
-    else:
-        await ctx.send("now playing: {0}".format(song))
-
-# drip command; plays among us drip
-@bot.command()
-async def drip(ctx):
-    try:
-        channel = ctx.author.voice.channel
-        await channel.connect()
-    except discord.errors.ClientException:
-        await ctx.send("Now playing: Among Us Drip")
-    except AttributeError:
-        await ctx.send("You're not connected to a voice channel.")
-    else:
-        await ctx.send("Now playing: Among Us Drip")
-    drip = discord.utils.get(bot.voice_clients)
-    drip.play(discord.FFmpegPCMAudio("drip.mp3"))
-
-# play command; plays song like a music bot
+# p command; makes bot play user-specified music
 @bot.command()
 async def p(ctx, *, msg):
+    # checks if bot is already connected to voice channel
+    try:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    # ClientException is thrown when bot is already connected, so it starts to search for song to play
+    except discord.errors.ClientException:
+        await ctx.send("Searching...")
+    # AttributeError is thrown when whoever wrote the command isn't in a voice channel, so stops operation of the command
+    except AttributeError:
+        await ctx.send("You're not connected to a voice channel.")
+        return
+    # otherwise, it joins voice channel and starts searching
+    else:
+        await ctx.send("Searching...")
+
+
+    # sets the voice channel the bot is referring back to
+    music = discord.utils.get(bot.voice_clients)
+
+    # uses YouTube_DL to start music play operations
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        # checks if message is a link
+        try:
+            get(msg)
+        # if an error is thrown, the message isn't a link and searches for the video on YouTube
+        except:
+            dict = ydl.extract_info(f"ytsearch:{msg}", download=False)['entries'][0]
+            song = (dict['webpage_url'])
+        # if no error is thrown, the message is a link so it uses it to download the song
+        else:
+            dict = ydl.extract_info(msg, download=False)
+            song = msg
+
+    # checks if there is a song @ song.mp3 and see if removable
     song_there = os.path.isfile("song.mp3")
     try:
         if song_there:
             os.remove("song.mp3")
+    # if irremovable, PermissionError is spit out, and can then be used to queue songs to it
     except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the `stop` command.")
+        queue.append(dict['title'])
+        links.append(dict['webpage_url'])
+        await ctx.send("Queued!")
         return
 
-    try:
-        channel = ctx.author.voice.channel
-        await channel.connect()
-    except discord.errors.ClientException:
-        await ctx.send("Searching...")
-    except AttributeError:
-        await ctx.send("You're not connected to a voice channel.")
-    else:
-        await ctx.send("Searching...")
-
-    music = discord.utils.get(bot.voice_clients)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'noplaylist': 'True',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '128',
-        }],
-    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            get(msg)
-        except:
-            dict = ydl.extract_info(f"ytsearch:{msg}", download=False)['entries'][0]
-            song = (dict['webpage_url'])
-        else:
-            song = msg
+        # song gets downloaded then replaced by song.mp3 so bot can play in vc
         dl = str(song)
         ydl.download([dl])
         for file in os.listdir("./"):
             if file.endswith(".mp3"):
                 os.rename(file, "song.mp3")
-    music.play(discord.FFmpegPCMAudio("song.mp3"))
+    music.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda x: next_song(ctx))
 
-    try:
-        title = (dict['title'])
-    except:
-        title = "your requested song" # incomplete
+    title = (dict['title'])
 
     await ctx.send("Now playing: " + title)
-    
-# join command; joins voice channel
+
+
+
+# q command; makes bot display the songs in queue
 @bot.command()
-async def join(ctx):
-    try:
-        channel = ctx.author.voice.channel
-        await channel.connect()
-    except discord.errors.ClientException:
-        await ctx.send("Already connected to channel!")
-    except AttributeError:
-        await ctx.send("You need to be connected to a voice channel to use this command.")
+async def q(ctx):
+    if len(queue) == 0:
+        await ctx.send("Queue is empty! Use `s!p <song name or song link>` to queue songs!")
     else:
-        await ctx.send("Connected!")
+        await ctx.send(queue)
 
-# leave command; leaves voice channel
-@bot.command()
-async def leave(ctx):
-    try:
-        channel = ctx.voice_client
-        await channel.disconnect()
-    except AttributeError:
-        await ctx.send("The bot is not connected to a voice channel.")
-    else:
-        await ctx.send("Successfully disconnected!")
-
-# pause command; pauses music playing
-@bot.command()
-async def pause(ctx):
-    music = discord.utils.get(bot.voice_clients)
-    if music.is_playing():
-        music.pause()
-        await ctx.send("Pausing!")
-    else:
-        await ctx.send("Currently paused!")
-
-# resume command; resumes music
-@bot.command()
-async def resume(ctx):
-    music = discord.utils.get(bot.voice_clients)
-    if music.is_paused():
-        music.resume()
-        await ctx.send("Resuming!")
-    else:
-        await ctx.send("Currently playing!")
-
-# stop command; stops playing music
-@bot.command()
-async def stop(ctx):
-    music = discord.utils.get(bot.voice_clients)
-    if music.is_playing():
-        music.stop()
-        await ctx.send("Stopping!")
-    elif music.is_paused:
-        await ctx.send("Music is currently stopped. Music must be playing to stop.")
-    else:
-        await ctx.send("There is no music playing.")
-
-# test command; tests random stuff
-@bot.command()
-async def test(ctx):
-    await ctx.send("your test subject here")
-
-# runs bot with bot token
-bot.run('your-token')
-
+# makes the bot run with the bot token
+bot.run('your-token-here')
