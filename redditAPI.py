4b6a18bd198f2badc5f768a038a9c486dac828eb
Joon <77248354+joonsauce@users.noreply.github.com>
2021-08-26 04:22:33 -0400

    added descriptive comments

--- redditAPI.py
+++ redditAPI.py
@@ -1,23 +1,38 @@
+# imports discord bot settings
 from setting import *
 
+# sets parameters for using AsyncPRAW
 reddit = asyncpraw.Reddit(
+    # replace the string with your client id
     client_id = '[your client id]',
+    # replace the string with your client secret
     client_secret = '[your client secret]',
+    # repalce the string with your user agent
     user_agent = '[your user agent]',
 )
 
 # susmeme command; sends random meme from r/amongusmemes
 @bot.command()
 async def susmeme(ctx):
+    # sets the subreddit the bot pulls the information from, just change the string to the subreddit you want
     subreddit = await reddit.subreddit("amongusmemes")
+    # gets a random post from the subreddit
     image_link = await subreddit.random()
+    # makes bot verify if the post is stickied or nsfw and that the link is an image
     if not image_link.stickied and image_link.over_18 is False \
             and image_link.url.endswith(('jpg', 'jpeg', 'png')):
+        # sets embed as discord embed
         embed = discord.Embed(
+            # sets color theme for embed
             colour=discord.Colour.red()
         )
+        # sets title of embed as the title of the post
         embed.set_author(name=image_link.title)
+        # sets the image as the image linked with the random post
         embed.set_image(url=image_link.url)
+        # makes bot send the embed as a message
         await ctx.send(embed=embed)
+    # if verification fails, send out an error message; this may be changed to repeat until it works
     else:
+        # makes bot send error message 
         await ctx.send("An error has occured, please try again later.")
