f87faf446a9e11ab992cb923bf4bf262f5b334d8
Joon <77248354+joonsauce@users.noreply.github.com>
2021-08-26 04:13:26 -0400

    added comments

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
-# various pacakges to be imported
+# links the main bot file with the other feature files
+# links to the redesigned help command
 from help import *
+# links to the random sus meme feature
 from redditAPI import *
+# links to the various bot settings
 from setting import *
+# links to the different "sus" features
 from sus import *
+# links to the soundboard features
 from music import *
 
 # test command; used for test purposes to debug things; just a short message for now but you can use it for whatever other purpose you want
 @bot.command()
 async def test(ctx):
+    # sends the message in the quote
     await ctx.send("You think you're funny huh?")
 
 # makes the bot run with the bot token
