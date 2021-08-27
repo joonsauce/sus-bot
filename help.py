d985d3b0264061eca461c7318deba83862741b9b
Joon <77248354+joonsauce@users.noreply.github.com>
2021-08-27 00:22:30 -0400

    fixed widespread typo, added forgotten command

--- help.py
+++ help.py
@@ -13,50 +13,54 @@ async def help(ctx, *, msg=''):
     embed.set_author(name="sus bot help")
     # if there is no message following the command, it displays all commands this bot has
     if msg == '':
-        embed.add_field(name="bal", value='Allows user to check how much susCash they have. Usage: s!bal', inline="False")
-        embed.add_field(name="drip", value='Plays Among Us Drip if user is connected to a voice channel. Usage: s!drip', inline="False")
-        embed.add_field(name="join", value='Makes bot join the voice channel the user is in. Usage: s!join', inline="False")
-        embed.add_field(name="leave", value='Makes bot leave the voice channel it is in. Usage: s!leave', inline="False")
-        embed.add_field(name="pp", value='Pauses and resumes music playing. Usage: s!pp', inline="False")
-        embed.add_field(name="roll", value='Makes bot run a simulated game of dice roll. Usage: s!roll', inline="False")
-        embed.add_field(name="scan", value='Makes bot run an Among Us style medbay scan. Usage: s!scan <user>*', inline="False")
-        embed.add_field(name="stop", value='Makes bot stop whatever music is playing. Usage: s!stop', inline="False")
-        embed.add_field(name="sus", value='Susses another user. Usage: s!sus <user> <action>', inline="False")
-        embed.add_field(name="susimg", value='Returns avatar of tagged user in Among Us character. Usage: s!susimg <user>*', inline="False")
-        embed.add_field(name="suslogo", value='Returns image of susbot Usage: s!suslogo', inline="False")
-        embed.add_field(name="susmeme", value='Returns a random meme from r/amongusmemes. Usage: s!susmeme', inline="False")
-        embed.add_field(name="susrate", value='Returns susrate of tagged user. Usage: s!susrate <user>*', inline="False")
+        embed.add_field(name="bal", value='Allows user to check how much susCash they have. Usage: s!bal', inline=False)
+        embed.add_field(name="drip", value='Plays Among Us Drip if user is connected to a voice channel. Usage: s!drip', inline=False)
+        embed.add_field(name="help", value="Displays the help menu. Usage: s!help", inline=False)
+        embed.add_field(name="join", value='Makes bot join the voice channel the user is in. Usage: s!join', inline=False)
+        embed.add_field(name="leave", value='Makes bot leave the voice channel it is in. Usage: s!leave', inline=False)
+        embed.add_field(name="pp", value='Pauses and resumes music playing. Usage: s!pp', inline=False)
+        embed.add_field(name="roll", value='Makes bot run a simulated game of dice roll. Usage: s!roll', inline=False)
+        embed.add_field(name="scan", value='Makes bot run an Among Us style medbay scan. Usage: s!scan <user>*', inline=False)
+        embed.add_field(name="stop", value='Makes bot stop whatever music is playing. Usage: s!stop', inline=False)
+        embed.add_field(name="sus", value='Susses another user. Usage: s!sus <user> <action>', inline=False)
+        embed.add_field(name="susimg", value='Returns avatar of tagged user in Among Us character. Usage: s!susimg <user>*', inline=False)
+        embed.add_field(name="suslogo", value='Returns the image of susbot logo. Usage: s!suslogo', inline=False)
+        embed.add_field(name="susmeme", value='Returns a random meme from r/amongusmemes. Usage: s!susmeme', inline=False)
+        embed.add_field(name="susrate", value='Returns susrate of tagged user. Usage: s!susrate <user>*', inline=False)
     # below is all of the separate commands you can get info about 
     elif msg == 'bal':
-        embed.add_field(name="bal", value='Allows user to check how much susCash they have. Usage: s!bal', inline="False")
+        embed.add_field(name="bal", value='Allows user to check how much susCash they have. Usage: s!bal',
+                        inline=False)
     elif msg == 'drip':
         embed.add_field(name="drip", value='Plays Among Us Drip if user is connected to a voice channel. Usage: s!drip',
-                        inline="False")
+                        inline=False)
+    elif msg == 'help':
+        embed.add_field(name="help", value="Displays the help menu. Usage: s!help", inline=False)
     elif msg == 'join':
         embed.add_field(name="join", value='Makes bot join the voice channel the user is in. Usage: s!join',
-                        inline="False")
+                        inline=False)
     elif msg == 'leave':
         embed.add_field(name="leave", value='Makes bot leave the voice channel it is in. Usage: s!leave',
-                        inline="False")
+                        inline=False)
     elif msg == 'pp':
-        embed.add_field(name="pp", value='Pauses and resumes music playing. Usage: s!pp', inline="False")
+        embed.add_field(name="pp", value='Pauses and resumes music playing. Usage: s!pp', inline=False)
     elif msg == 'roll':
-        embed.add_field(name="roll", value='Makes bot run a simulated game of dice roll. Usage: s!roll', inline="False")
+        embed.add_field(name="roll", value='Makes bot run a simulated game of dice roll. Usage: s!roll', inline=False)
     elif msg == 'scan':
         embed.add_field(name="scan", value='Makes bot run an Among Us style medbay scan of the user. Usage: s!scan',
-                        inline="False")
+                        inline=False)
     elif msg == 'stop':
-        embed.add_field(name="stop", value='Makes bot stop whatever music is playing. Usage: s!stop', inline="False")
+        embed.add_field(name="stop", value='Makes bot stop whatever music is playing. Usage: s!stop', inline=False)
     elif msg == 'sus':
-        embed.add_field(name="sus", value='Susses another user. Usage: s!sus <user> <action>', inline="False")
+        embed.add_field(name="sus", value='Susses another user. Usage: s!sus <user> <action>', inline=False)
     elif msg == 'susimg':
-        embed.add_field(name="susimg", value='Returns avatar of tagged user in Among Us character. Usage: s!susimg <user>*', inline="False")
+        embed.add_field(name="susimg", value='Returns avatar of tagged user in Among Us character. Usage: s!susimg <user>*', inline=False)
     elif msg == 'suslogo':
-        embed.add_field(name="suslogo", value='Returns image of susbot Usage: s!suslogo', inline="False")
+        embed.add_field(name="nathansus", value='Returns the image of susbot logo. Usage: s!suslogo', inline=False)
     elif msg == 'susmeme':
-        embed.add_field(name="susmeme", value='Returns a random meme from r/amongusmemes. Usage: s!susmeme', inline="False")
+        embed.add_field(name="susmeme", value='Returns a random meme from r/amongusmemes. Usage: s!susmeme', inline=False)
     elif msg == 'susrate':
-        embed.add_field(name="susrate", value='Returns susrate of tagged user. Usage: s!susrate <user>*', inline="False")
+        embed.add_field(name="susrate", value='Returns susrate of tagged user. Usage: s!susrate <user>*', inline=False)
     
     # adds line indicating that a star means the element is optional
     embed.add_field(name="OTHER THINGS TO NOTE", value='Any element with a * beside it means the element is optional.')
