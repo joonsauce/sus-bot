2eefe501004f3393c1170028ed869a56aed67826
Joon <77248354+joonsauce@users.noreply.github.com>
2021-08-29 22:36:43 -0400

    setting up new features

---
+++ among_us_api.py
@@ -0,0 +1,7 @@
+# import bot settings
+from setting import *
+
+# use whichever one, i used edge driver just because the developers of edge (Microsoft)
+# had official webdrivers
+driver = webdriver.Edge('./edgedriver')
+# driver = webdriver.Crhome('./chromedriver')
