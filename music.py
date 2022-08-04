26c0afd4143ffdeac6577c7fcac11e7a80aba7fb
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-08-03 23:26:13 -0400

    updated music function & removed unusable code

--- music.py
+++ music.py
@@ -1,157 +1,94 @@
 # imports all bot settings
 from setting import *
 
+# saves the servers the bot is connected to & the channels in each server
+players = {}
+
 # join command; makes the bot join a voice channel; for debugging purposes
 @bot.command()
 async def join(ctx):
-    # checks if the user is in a voice channel and tries to connect to it
-    try:
-        # sets channel as the voice channel the user is in
-        channel = ctx.author.voice.channel
-        # connects to the voice channel
-        await channel.connect()
-    # if the bot throws a ClientException error, it likely indicates the bot is already in the voice channel, so sends error message
-    except discord.errors.ClientException:
-        # sends error message
-        await ctx.send("The bot is already connected to the voice channel! Code: sbjoin_botInChannel")
-    # if the bot throws an AttributeError, it likely means the user isn't connected to a voice channel, so sends an error message
-    except AttributeError:
-        # sends error message
-        await ctx.send("You need to be connected to a voice channel to use this command. Code: sbjoin_noOneInChannel")
-    # if no errors are thrown the bot indicates success
+    # checks if user in voice channel
+    if ctx.author.voice is not None:
+        # if bot not in channel together
+        if discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild) is None:
+            # bot joins channel and sends success message
+            await ctx.author.voice.channel.connect()
+            await ctx.send("Connected!")
+        else:
+            # otherwise sends message to indicate bot already in channel
+            await ctx.send("The bot is already connected to the voice channel!")
+    # otherwise, alert user to join voice channel first
     else:
-        # sends success message
-        await ctx.send("Connected!")
+        await ctx.send("You need to be connected to a voice channel to use this command.")
 
 # leave command; makes the bot leave a voice channel
 @bot.command()
 async def leave(ctx):
     # checks if bot is in a voice channel
-    try:
-        # sets channel as the voice channel the bot is connected to
-        channel = ctx.voice_client
-        # makes bot leave the voice channel
-        await channel.disconnect()
-    # if the bot throws an AttributeError the bot is likely not connected to a voice channel (already disconnected) so throws an error
-    except AttributeError:
-        # sends error message
-        await ctx.send("The bot is currently not connected to a voice channel. Code: sbleav_botNotInChannel")
-    # if no errors are thrown the bot indicates success
+    if discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild).is_connected:
+        # if the server's id is in the players dicts, it means all other dicts have something in it, so it empties the dicts
+        if ctx.guild.id in players:
+            players.pop(ctx.guild.id)
+        # disconnects the bot and sends success message
+        await ctx.message.guild.voice_client.disconnect()
+        await ctx.send("Disconnected!")
+    # otherwise, send error message that the bot is not connected to a voice channel
     else:
-        # sends success message
-        await ctx.send("Successfully disconnected!")
+        await ctx.send("The bot is not connected to a voice channel.")
 
 # pp command; makes the bot pause/play the song
 @bot.command()
 async def pp(ctx):
-    # checks if bot is in a voice channel
-    try:
-        # tries to set the voice channel as channel
-        channel = ctx.voice_client
-    # if AttributeError is thrown, bot it not connected to voice channel
-    except AttributeError:
-        # sends error message
-        ctx.send("The bot is currently not connected to a voice channel. Code: sbpp_botNotInChannel")
-    # if bot in voice channel
-    else:
-        # sets the voice channel the bot is referring back to
-        vc = discord.utils.get(bot.voice_clients)
-        # checks if the bot is playing anything
-        if vc.is_playing():
-            # pauses whatever is being played
-            vc.pause()
-            # sends success message
-            await ctx.send("Pausing!")
-        # checks if the bot has anything paused
-        elif vc.is_paused:
-            # resumes whatever is paused
-            vc.resume()
-            # sends success message
-            await ctx.send("Resuming!")
-        # if neither of those conditions are met, nothing is playing so it sends message
+    # checks if the bot is playing anything in the server
+    if ctx.guild.id in players:
+        # if the bot is playing something, pause
+        if players[ctx.guild.id].is_playing():
+            players[ctx.guild.id].pause()
+            await ctx.send("Pausing...")
+        # if the bot is paused, resume
+        elif players[ctx.guild.id].is_paused():
+            players[ctx.guild.id].resume()
+            await ctx.send("Resuming...")
+        # send error message
         else:
-            # sends nothing is playing message
-            await ctx.send("Nothing is currently playing! Code: sbpp_nothingPlaying")
+            await ctx.send("Nothing is currently playing!")
+    # send error message
+    else:
+        await ctx.send("The bot is currently not connected to a voice channel.")
 
-# stop command; makes the bot stop playing music
+# stop command; makes the bot stop playing music (can be used to skip as well if using queue system)
 @bot.command()
 async def stop(ctx):
-    # checks if bot is in a voice channel
-    try:
-        # tries to set the voice channel as channel
-        channel = ctx.voice_client
-    # if AttributeError is thrown, bot it not connected to voice channel
-    except AttributeError:
-        # sends error message
-        ctx.send("The bot is currently not connected to a voice channel. Code: sbstop_botNotInChannel")
-    # if bot in voice channel
-    else:
-        # sets the voice channel the bot is referring back to
-        vc = discord.utils.get(bot.voice_clients)
-        # checks if anything is playing in the voice channel
-        if vc.is_playing():
-            # stops music from playing
-            vc.stop()
-            # sends success message
-            await ctx.send("Stopping!")
-        # if something is paused, the bot indicates that there must be something playing to stop
-        elif vc.is_paused():
-            # sends error message
-            await ctx.send("Music is currently paused. Music must be playing to stop. Code: sbstop_soundPaused")
-        # if nothing is playing send error message
+    # if bot is playing something
+    if ctx.guild.id in players:
+        # if the bot is playing something, pause
+        if players[ctx.guild.id].is_playing():
+            players[ctx.guild.id].stop()
+            await ctx.send("Stopping...")
+        # if the bot is paused, resume
+        elif players[ctx.guild.id].is_paused():
+            players[ctx.guild.id].stop()
+            await ctx.send("Stopping...")
+        # send error message
         else:
-            # send error message
-            await ctx.send("Nothing is currently playing. Code: sbstop_nothingPlaying")    
-   
+            await ctx.send("Nothing is currently playing!")
+    # send error message
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
 
 # drip command; plays among us drip
 @bot.command()
 async def drip(ctx):
-    # checks if the user is a bot and sends message
-    if ctx.author.bot:
-        # sends a welcome message
-        await ctx.send("You're a bot, screw you! Code: sbdrip_notHumanException")
-    # otherwise the code continues
+    # checks if user in voice channel
+    if ctx.author.voice is not None:
+        # if bot not in channel together
+        if discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild) is None:
+            # bot joins channel
+            await ctx.author.voice.channel.connect()
+        # sets the voice channel to play in and plays the set song
+        player = ctx.message.voice_client
+        player.play(discord.FFmpegPCMAudio("music/drip.mp3"))
+        await ctx.send("Now playing: `Among Us Drip`")
+    # otherwise, alert user to join voice channel first
     else:
-        # checks if bot is in voice channel
-        try:
-            # sets channel as the user's voice channel
-            channel = ctx.author.voice.channel
-            # connects to user's voice channel
-            await channel.connect()
-        # if bot throws a ClientException, it means bot is already connected to voice channel so continues as normal
-        except discord.errors.ClientException:
-            # tries to play song
-            try:
-                # sets the voice channel the bot will play sound in
-                drip = discord.utils.get(bot.voice_clients)
-                # plays sound fild
-                drip.play(discord.FFmpegPCMAudio("music/drip.mp3"))
-            # if there is any error, throws error
-            except:
-                # sends error message
-                await ctx.send("There has been an error. Please try again later. Code: sbdrip_err1")
-            # if no errors, plays sound
-            else:
-                # sends success messages
-                await ctx.send("Now playing: Among Us Drip")
-        # if bot throws an AttributeError, it means user isn't connected to a voice channel and sends error message
-        except AttributeError:
-            # sends error message
-            await ctx.send("You're not connected to a voice channel.")
-        # if no errors are thrown the code continues as normal
-        else:
-            # tries to play song
-            try:
-                # sets the voice channel the bot will play sound in
-                drip = discord.utils.get(bot.voice_clients)
-                # plays sound fild
-                drip.play(discord.FFmpegPCMAudio("music/drip.mp3"))
-            # if there is any error, throws error
-            except:
-                # sends error message
-                await ctx.send("There has been an error. Please try again later. Code: sbdrip_err2")
-            # if no errors, plays sound
-            else:
-                # sends success messages
-                await ctx.send("Now playing: Among Us Drip")
+        await ctx.send("You need to be connected to a voice channel to use this command.")
