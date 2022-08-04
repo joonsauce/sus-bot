aebee49fd0de90385d63545423fcb2d346c5ac9a
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-08-03 23:37:20 -0400

    made roll command much more efficient

--- roll.py
+++ roll.py
@@ -6,69 +6,60 @@ from roll_functions import *
 @bot.command()
 async def roll(ctx, *, msg=''):
     if not msg:
-        await ctx.send("Please enter the amount of susCash you wish to use. Code: sbroll_nosusCashEntered")
+        await ctx.send("Please enter the amount of susCash you wish to use.")
     else:
-        try:
-            bet = int(msg)
-        except ValueError:
-            await ctx.send("The amount of susCash you entered is not a number. Code: sbroll_wrongsusCash")
-        else:
-            if bet <= 0:
-                await ctx.send("Please enter a positive number to bet.")
-            else:
+        if msg.isnumeric():
+            if float(msg).is_integer():
+                if int(msg) <= 0:
+                    await ctx.send("Please enter a positive value of susCash to use.")
+                    return
                 data = getRollData()
                 if data == -1:
-                    await ctx.send("There has been an error. Please try again later. Code: sbroll_getRollData")
-                else:
+                    await ctx.send("There has been an error. Please try again later.")
+                    return
+                user_there = findUser(str(ctx.author.id))
+                if user_there == -1:
+                    await ctx.send("There has been an error. Please try again later.")
+                    return
+                if user_there == -2:
+                    response = addUser(str(ctx.author.id))
+                    if response == -1:
+                        await ctx.send("There has been an error. Please try again later.")
+                        return
                     user_there = findUser(str(ctx.author.id))
-                    if user_there == -1:
-                        await ctx.send("There has been an error. Please try again later. Code: sbroll_findUser")
-                    elif user_there == -2:
-                        response = addUser(str(ctx.author.id))
-                        if response == -1:
-                            await ctx.send("There has been an error. Please try again later. Code: sbroll_addUser")
-                        else:
-                            user_there = findUser(str(ctx.author.id))
-                            if user_there == -1:
-                                await ctx.send("There has been an error. Please try again later. Code: sbroll_findUser")
-                            elif user_there == -2:
-                                await ctx.send(
-                                    "There has been an error. Please try again later. Code: sbroll_unknownfindUser")
-                            else:
-                                data = getRollData()
-                                if data == -1:
-                                    await ctx.send(
-                                        "There has been an error. Please try again later. Code: sbroll_getRollData")
-                                else:
-                                    result = verifyResults(data, user_there, bet)
-                                    await ctx.send(result)
-                    else:
-                        result = verifyResults(data, user_there, bet)
-                        await ctx.send(result)
+                    if user_there == -1 or user_there == -2:
+                        await ctx.send("There has been an error. Please try again later.")
+                        return
+                    data = getRollData()
+                    if data == -1:
+                        await ctx.send("There has been an error. Please try again later.")
+                        return
+                    result = verifyResults(data, user_there, int(msg))
+                    await ctx.send(result)
 
 # bal command; allows user to check how much susCash they have
 @bot.command()
 async def bal(ctx):
     data = getRollData()
     if data == -1:
-        await ctx.send("There has been an error. Please try again later. Code: sbbal_getRollData")
+        await ctx.send("There has been an error. Please try again later.")
     else:
         user_there = findUser(str(ctx.author.id))
         if user_there == -1:
-            await ctx.send("There has been an error. Please try again later. Code: sbbal_findUser")
+            await ctx.send("There has been an error. Please try again later.")
         elif user_there == -2:
             response = addUser(str(ctx.author.id))
             if response == -1:
-                await ctx.send("There has been an error. Please try again later. Code: sbbal_addUser")
+                await ctx.send("There has been an error. Please try again later.")
             else:
                 user_there = findUser(str(ctx.author.id))
                 if user_there == -1:
-                    await ctx.send("There has been an error. Please try again later. Code: sbroll_findUser")
+                    await ctx.send("There has been an error. Please try again later.")
                 else:
                     pass
         data = getRollData()
         if data == -1:
-            await ctx.send("There has been an error. Please try again later. Code: sbroll_getRollData")
+            await ctx.send("There has been an error. Please try again later.")
         else:
             total = int(data["records"][user_there]["fields"]["sus"])
             await ctx.send("You currently have {0} susCash under your account.".format(total))
