67cfb353c819b3fd524710279e5370de2cc876c0
joonsauce <77248354+joonsauce@users.noreply.github.com>
2024-01-02 22:26:47 -0500

    v2.0.0.1: Remove 'test' command

--- bot.py
+++ bot.py
@@ -6,10 +6,5 @@ from setting import *
 from sus import *
 from music import *
 
-# test command; used for test purposes to debug things; just a short message for now but you can use it for whatever other purpose you want
-@bot.command()
-async def test(ctx):
-    await ctx.send("You think you're funny huh?")
-
 # makes the bot run with the bot token
 bot.run(bot_token)
